04c80fd990734224e33beb3ad56ba43f93138193
Daniel Nogueira <daninvasc2@gmail.com>
2023-02-07 20:44:19 -0200

    Criado opcao de editar

--- assets/js/cadastro_contas.js
+++ assets/js/cadastro_contas.js
@@ -1,5 +1,9 @@
 const form = document.querySelector('#contasForm');
 const tableBody = document.querySelector('#contasTable tbody');
+const codigoConta = document.querySelector('#codigo');
+const nomeConta = document.querySelector('#nome');
+const btnSubmit = document.querySelector('#btn-submit');
+const modalExcluir = document.querySelector('#modalExcluir');
 
 document.onload = atualizarTabela();
 
@@ -10,9 +14,25 @@ form.addEventListener('submit', function (event) {
     for (let [key, value] of formData) {
         conta[key] = value;
     }
-    console.log(conta);
 
     const contas = getFromLocalStorage('contas') || [];
+
+    if (btnSubmit.value == 'Atualizar') {
+        const novaLista = contas.map(function (conta) {
+            if (conta.codigo == codigoConta.value) {
+                conta.nome = nomeConta.value;
+            }
+            return conta;
+        });
+
+        saveToLocalStorage('contas', novaLista);
+        btnSubmit.value = 'Cadastrar';
+        btnSubmit.textContent = 'Cadastrar';
+        form.reset();
+        atualizarTabela();
+        return;
+    }
+
     contas.push(conta);
     saveToLocalStorage('contas', contas);
 
@@ -29,7 +49,10 @@ function atualizarTabela() {
             <td>${conta.codigo}</td>
             <td>${conta.nome}</td>
             <td>
-                <button class="btn btn-danger btn-sm" onclick="excluirConta(${conta.codigo})">
+                <button class="btn btn-secondary btn-sm" onclick="editarConta(${conta.codigo})">
+                    Editar
+                </button>
+                <button class="btn btn-danger btn-sm" data-toggle="modal" data-target="#exampleModal" onclick="excluirConta(${conta.codigo})">
                     Excluir
                 </button>
             </td>
@@ -45,4 +68,17 @@ function excluirConta(codigo) {
     });
     saveToLocalStorage('contas', novaLista);
     atualizarTabela();
+}
+
+function editarConta(codigo) {
+    const contas = getFromLocalStorage('contas') || [];
+    const conta = contas.find(function (conta) {
+        return conta.codigo == codigo;
+    });
+
+    codigoConta.value = conta.codigo;
+    nomeConta.value = conta.nome;
+
+    btnSubmit.value = 'Atualizar';
+    btnSubmit.textContent = 'Atualizar';
 }
