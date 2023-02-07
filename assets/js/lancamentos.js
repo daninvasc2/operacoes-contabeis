14fdeb2be9b726488e7e6f40c38c1477318a1cee
Henrique Lessa <henriquelessa123@gmail.com>
2023-02-07 20:25:01 -0300

    cadastro de lancamento transacoes

--- assets/js/lancamentos.js
+++ assets/js/lancamentos.js
@@ -1,15 +1,16 @@
 const codigo_credora = document.getElementById('codigo-credora');
 const codigo_devedora = document.getElementById('codigo-devedora');
+const form = document.getElementById('lancForm');
 
 codigo_credora.addEventListener('blur', () => {
     let codigo = codigo_credora.value;
     let contas = [];
-    if(codigo != ""){
+    if (codigo != "") {
         contas = filtrarContaPorCodigo(codigo);
     }
-    if(contas.length > 0){
+    if (contas.length > 0) {
         document.getElementById('nome-credora').value = contas[0].nome;
-    }else{
+    } else {
         document.getElementById('nome-credora').value = "Conta não encontrada";
     }
 });
@@ -17,12 +18,29 @@ codigo_credora.addEventListener('blur', () => {
 codigo_devedora.addEventListener('blur', () => {
     let codigo = codigo_devedora.value;
     let contas = [];
-    if(codigo != ""){
+    if (codigo != "") {
         contas = filtrarContaPorCodigo(codigo);
     }
-    if(contas.length > 0){
+    if (contas.length > 0) {
         document.getElementById('nome-devedora').value = contas[0].nome;
-    }else{
+    } else {
         document.getElementById('nome-devedora').value = "Conta não encontrada";
     }
 });
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const formData = new FormData(form);
+    const lancamento = {};
+    for (let [key, value] of formData) {
+        lancamento[key] = value;
+    }
+    console.log(lancamento);
+
+    const lancamentos = getFromLocalStorage('lancamentos') || [];
+    lancamentos.push(lancamento);
+    saveToLocalStorage('lancamentos', lancamentos);
+
+    // atualizarTabela();
+    form.reset();
+})
