3db6c67a5ac21470929e50eacdd69173d760299d
ThiagoRocha14 <thiagormnb@gmail.com>
2023-02-14 21:07:42 -0200

    adicionando relatorio de plano de conta

--- assets/js/relatorio_pc.js
+++ assets/js/relatorio_pc.js
@@ -1,1 +1,15 @@
-const resultadoRelatorio = document.querySelector('#resultadoRelatorio');
+const tableBody = document.querySelector('#contasTable tbody');
+
+function atualizarTabela() {
+    let contas = getFromLocalStorage('contas') || [];
+
+    tableBody.innerHTML = '';
+    contas.forEach(function (conta) {
+        const tr = document.createElement('tr');
+        tr.innerHTML = `
+            <td>${conta.codigo}</td>
+            <td>${conta.nome}</td>
+        `;
+        tableBody.appendChild(tr);
+    });
+}
